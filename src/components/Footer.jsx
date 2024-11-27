9cfae078312d66f127cf9c3bb38c2bc5adeed1ff
VincenzoIuliano <bubucane@outlook.it>
2024-11-27 17:22:47 +0100

    EDIT: fix in css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="page-footerd">
-            <div className="page-footer">
+        <footer className="page-footer">
+            <div>
                 <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, quas mollitia necessitatibus rem odio itaque voluptatem voluptas labore natus quos? Earum ducimus adipisci officiis corporis laboriosam reiciendis quasi blanditiis recusandae!</span>
             </div>
         </footer>
