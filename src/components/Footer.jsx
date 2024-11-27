40a943c86c403f73fbcc23af36d19381ea109c5f
VincenzoIuliano <bubucane@outlook.it>
2024-11-27 17:44:55 +0100

    FIX: CSS fixed, not yet ended

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 export default function Footer() {
     return (
-        <footer className="page-footer">
-            <div>
-                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, quas mollitia necessitatibus rem odio itaque voluptatem voluptas labore natus quos? Earum ducimus adipisci officiis corporis laboriosam reiciendis quasi blanditiis recusandae!</span>
-            </div>
-        </footer>
-    )
+      <footer className="page-footer">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, quas
+          mollitia necessitatibus rem odio itaque voluptatem voluptas labore
+          natus quos? Earum ducimus adipisci officiis corporis laboriosam
+          reiciendis quasi blanditiis recusandae!
+        </p>
+      </footer>
+    );
 }
